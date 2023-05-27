750cc4a70538b829d8cb69c3740626c128d8ece1
SamiMori <moriwakisami@gmail.com>
2023-05-27 13:58:15 -0300

    adding dist

--- src/Pages/Personagem.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <article class="flex justify-center">
-    <div class="max-w-[1000px] flex flex-col gap-4">
-      <h2 class="text-lg font-bold">Criação de Personagem</h2>
-    </div>
-  </article>
-</template>
